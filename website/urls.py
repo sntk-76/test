185252f60e491013103c0e05c69b3be652bfb3ff
sntk-76 <sn.tvkl@gmail.com>
2022-08-11 15:05:53 +0430

    render html file
    add html file with templates

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import http_test,json_test
+from website.views import home,contact,about
 
 urlpatterns = [
-    path('',http_test),
-    path('json-test',json_test)
+    path('',home),
+    path('contact',contact),
+    path('about',about)
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-def http_test(request):
-    return HttpResponse('<h1> Hi dear</h1>')
-def json_test(request):
-    return JsonResponse({'sina' : '25'})    
+def home(request):
+    return render(request,'web page\home.html')
+def contact(request):
+    return render(request,'web page\contact.html')
+def about(request):
+    return render(request,'web page\About.html')
