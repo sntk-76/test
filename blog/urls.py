2902ba4f80ab2845ecbd7637eb4b47d1c29acd7f
sntk-76 <sn.tvkl@gmail.com>
2022-08-13 00:19:32 +0430

    Revert "creating a complete page"
    This reverts commit d10722c02b3252e7d9b9a93d944e788beb7c6bad.

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from blog.views import blog_home,blog_single
-app_name = 'blog'
-
-urlpatterns = [
-    path('blog-home',blog_home,name='blog-home'),
-    path('blog-single',blog_single,name='blog-single')
-]
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,8 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('website.urls')),
-    path('',include('blog.urls'))
+    path('',include('website.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
